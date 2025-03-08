00572468d01bb6548c0cd1395a91eb680933a57e
Joe <mat.joe1812@gmail.com>
2025-03-08 14:26:32 +0200

    Best Time to Buy and Sell Stock

--- Easy/121.py
+++ Easy/121.py
@@ -1,8 +1,23 @@
-from pymongo.message import MIN_INT32, MAX_INT32
-
-
 class Solution(object):
     def maxProfit(self, prices):
-        min = MIN_INT32
-        max = MAX_INT32
+        min_price = float('inf')
+        max_profit = 0
+        for price in prices:
+            min_price = min(min_price, price)
+            profit = price - min_price
+            max_profit = max(profit, max_profit)
+
+        return max_profit
+    def maxProfitSecond(self, prices):
+        left = 0
+        right = 1
+        max_profit = 0
+
+        while right > len(prices):
+            if (prices[right] > prices[left]):
+                max_profit = max(max_profit, prices[right] - prices[left])
+            right += 1
+            left += 1
+        return max_profit
+
 
