6a2c372100e1f8ccc92f84e031ae9797273230fa
Joe <mat.joe1812@gmail.com>
2025-03-09 18:04:08 +0200

    Jump Game II

--- LeetCode/Medium/45.py
+++ LeetCode/Medium/45.py
@@ -4,4 +4,12 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        
+        count_jumps = 0
+        cur_end = 0 # Текущая граница прыжка
+        max_reach = 0
+        for k in range(len(nums) - 1):
+            max_reach = max(max_reach, nums[k] + k)
+            if k == cur_end:
+                cur_end = max_reach
+                count_jumps += 1
+        return count_jumps
